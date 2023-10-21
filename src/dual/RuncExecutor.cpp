de8ba819f845754ddc950d791097bc155aec01c1
Petr <pprok@tacita.net>
2023-10-21 09:55:47 +0300

    enumerate covers by batches with python api

--- src/dual/RuncExecutor.cpp
+++ src/dual/RuncExecutor.cpp
@@ -1,46 +1,88 @@
 #include "RuncExecutor.h"
-#include "Counters.h"
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
 #include "RUNC.h"
 
-extern "C"
-
-        class DualizerState {
+class DualizerState {
 public:
     Runc::GlobalState state;
-    Counters counters;
     bit_matrix input_matrix;
 
-    DualizerState(int32_t m, int32_t n) {
-        state.InnerCallback = &counters;
-        state.CoverCallback = &counters;
-        input_matrix.add_rows(m, n);
-    };
+    uint32_t covers_buffer_size = 0;
+    uint32_t* covers_buffer = nullptr;
+
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::unique_ptr<std::thread> thread_;
+    std::atomic<bool> stop_ = false;
+
+    IDualizationCallbackPtr cover_callback;
+
+    DualizerState() {
+        cover_callback = dualization_callback([this](DualizationNode& node){
+            process_cover(node.columns);
+            if (stop_) {
+                node.stoped = true;
+            }
+        });
+    }
+
+    void process_cover(std::vector<int> const& cover){
+        std
+    }
 
     void set_input_one(int32_t i, int32_t j) {
         input_matrix.set(i, j);
     }
 
-    void dualize() {
+    void start_dualization() {
         state.SetMatrix(std::move(input_matrix));
-        state.Dualize();
+
+        thread_ = std::make_unique<std::thread>([this]{
+            state.Dualize();
+        });
+    }
+
+    ~DualizerState(){
+        stop_ = true;
+        cv_.notify_all();
+        if (thread_){
+            thread_->join();
+        }
     }
 };
 
-DualizerHandler init_dualizer(int32_t m, int32_t n) {
-    return new DualizerState(m, n);
+extern "C" {
+
+DualizerHandler init_dualizer() {
+    return new DualizerState();
 }
 
 void release_dualizer(DualizerHandler dualizer) {
     delete reinterpret_cast<DualizerState*>(dualizer);
 }
 
-void dualize(DualizerHandler dualizer) {
-    reinterpret_cast<DualizerState*>(dualizer)->dualize();
+int dualize(DualizerHandler dualizer) {
+    auto p_state = reinterpret_cast<DualizerState*>(dualizer);
+    p_state->dualize();
 }
 
 void set_input_row(DualizerHandler dualizer, int32_t row_num, int32_t ones_num, int32_t* ones) {
     auto& state = *reinterpret_cast<DualizerState*>(dualizer);
     for (int j = 0; j < ones_num; ++j) {
-        state.input_matrix.set(row_num, ones[j], true);
+        state.input_matrix.set(row_num, ones[j]);
     }
+}
+}
+uint32_t enumerate_covers(DualizerHandler dualizer, uint32_t buffer_size, uint32_t* covers){
+
+    auto& state = *reinterpret_cast<DualizerState*>(dualizer);
+
+
+
+    return 0;
 }
--- src/dual/runc.py
+++ src/dual/runc.py
@@ -1,5 +1,7 @@
 import ctypes
 
+import numpy
+
 mylib = ctypes.CDLL("/home/user/dual/cmake-build-debug/src/dual/librunc.so")
 
 mylib.init_dualizer.restype = ctypes.c_void_p
@@ -7,9 +9,18 @@ mylib.init_dualizer.argtypes = [ctypes.c_int, ctypes.c_int]
 
 mylib.release_dualizer.argtypes = [ctypes.c_void_p]
 
-dualizer = mylib.init_dualizer(30, 40)
+mylib.set_input_row.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int,
+                                numpy.ctypeslib.ndpointer(dtype=numpy.int32)]
 
-print(dualizer)
+mylib.dualize.argtypes = [ctypes.c_void_p]
+mylib.dualize.restype = ctypes.c_int
+
+dualizer = mylib.init_dualizer(0, 40)
+
+row = numpy.array([0, 4, 7, 10], dtype=numpy.int32)
+mylib.set_input_row(dualizer, 0, len(row), row)
+
+print(mylib.dualize(dualizer))
 
 mylib.release_dualizer(dualizer)
 
